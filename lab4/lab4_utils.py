c9e79c6afd313f29cd537f7aac64c3365345314b
Bastien Soucasse <bastien.soucasse@icloud.com>
2022-10-26 17:03:10 +0200

    Fix: Ten Worst for Keras

--- lab4/lab4_utils.py
+++ lab4/lab4_utils.py
@@ -1,19 +1,13 @@
-import os
-import time
-
 import keras
 import matplotlib
 import matplotlib.pyplot as plt
 import numpy as np
-from keras.datasets import cifar10, mnist
-
 
 
 def ten_worst(dataset, num_classes, y_pred, save, path=None):
     '''
     Find 10 worst classified images, from predictions
-        dataset: dataset to work on
-                here, should becifar10 or mnist
+        dataset: dataset to work on here, should be cifar10 or mnist
 
         num_classes: the number of classes of dataset (int)
 
@@ -21,13 +15,11 @@ def ten_worst(dataset, num_classes, y_pred, save, path=None):
 
         save: True if 10 worst images need to be saved, False otherwise (boolean)
 
-        path: path to where you should save the images (string)
-              here, should be ex_num/keras_or_pytorch/model
-              doesn't need to be defined if save is False 
+        path: path to where you should save the images (string) here, should be ex_num/keras_or_pytorch/model doesn't need to be defined if save is False 
     '''
-    
+
     # Load test data.
-    _, (x_test, y_test) = mnist.load_data()
+    _, (x_test, y_test) = dataset.load_data()
     height, width = x_test.shape[1], x_test.shape[2]
     y_test = keras.utils.to_categorical(y_test, num_classes)
 
@@ -35,11 +27,10 @@ def ten_worst(dataset, num_classes, y_pred, save, path=None):
     real_classes = np.argmax(y_test, axis=1)
     pred_classes = np.argmax(y_pred, axis=1)
 
-    # Build array of tuple of misclassified images indices, 
+    # Build array of tuple of misclassified images indices,
     # with their predicted class, real class and probability predicted for their real class
-    misclassified = [(i, pred_classes[i], real_classes[i], y_pred[i][real_classes[i]])
-                      for i in range(y_test.shape[0]) if real_classes[i] != pred_classes[i]]
-    
+    misclassified = [(i, pred_classes[i], real_classes[i], y_pred[i][real_classes[i]]) for i in range(y_test.shape[0]) if real_classes[i] != pred_classes[i]]
+
     # Sort it by probabilities and retrieve only the ten worst.
     misclassified.sort(key=lambda a: a[3])
     ranking = misclassified[-10:]
@@ -51,5 +42,8 @@ def ten_worst(dataset, num_classes, y_pred, save, path=None):
         i, pc, rc, _ = ranking[x-1]
         print(f'{x}. IMAGE {i}\n    - Predicted category: {pc}\n    - Actual category: {rc}\n')
         if save:
-            plt.imsave('ten_worst/%s/%d.png' % (path, x), x_test[i,:].reshape(28,28), cmap = matplotlib.cm.binary)
-    
+            plt.imsave('ten_worst/%s/%d.png' % (path, x), x_test[i, :].reshape(28, 28), cmap=matplotlib.cm.binary)
+
+
+def ten_worst():
+    return
